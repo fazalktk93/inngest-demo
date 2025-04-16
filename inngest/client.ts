136a6abc55bb79a30fed106220eb5d15979650a6
fazalktk93 <fazalktk93@gmail.com>
2025-04-16 22:38:07 +0500

    dsfss

--- inngest/client.ts
+++ inngest/client.ts
@@ -5,10 +5,10 @@ import type {
 } from '@/inngest/customerOnboarding';
 import { EventSchemas, Inngest } from 'inngest';
 
+// Initialize Inngest without the eventKey for local development
 const inngest = new Inngest({
   id: 'Inngest Demo',
   schemas: new EventSchemas().fromUnion<AppCSVFileUploaded | AppUserSignedUp | AppEmailOpened>(),
-  eventKey: process.env.NEXT_PUBLIC_INNGEST_EVENT_KEY,
 });
 
 export default inngest;
